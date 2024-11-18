bc0742411d486d62577b6126cfc55c90b7792503
Joey Ciechanowicz <1148637+joeyciechanowicz@users.noreply.github.com>
2024-11-18 21:33:29 +0000

    delete ts

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import { render, Text, Box, Newline } from 'ink';
-import SelectInput from 'ink-select-input';
-import App from './app.js';
-
-
-render(<App />);
--- src/util.ts
+++
@@ -1,4 +0,0 @@
-
-export interface SelectedItem {
-    value: string;
-}
